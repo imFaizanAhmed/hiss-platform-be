71fcb44e21f06f01f6637246b290df552a56e086
imFaizanAhmed <imfaizanahmed6@gmail.com>
2024-02-05 00:43:54 +0500

    basic schema is created

--- src/schemas/creators.schema.ts
+++ src/schemas/creators.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { baseSchema } from './basic.schema';
 
 export type CreatorDocument = HydratedDocument<Creator>;
 
 @Schema()
-export class Creator {
+export class Creator extends baseSchema {
 
   @Prop({ required: true})
   firstName: string;
