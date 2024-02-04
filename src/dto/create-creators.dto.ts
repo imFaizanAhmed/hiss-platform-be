b0da0acc971316a36d1585dd1818324162c79ba7
imFaizanAhmed <imfaizanahmed6@gmail.com>
2024-02-05 00:44:42 +0500

    class validators are added

--- src/dto/create-creators.dto.ts
+++ src/dto/create-creators.dto.ts
@@ -1,8 +1,21 @@
+import { IsString, IsInt, IsEmail } from 'class-validator';
+
 export class CreateCreatorDto {
+  @IsString()
   firstName: string;
+
+  @IsString()
   middleName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsString()
   title: string;
+
+  @IsEmail()
   email: string;
+
+  @IsInt()
   phoneNumbe: number;
 }
