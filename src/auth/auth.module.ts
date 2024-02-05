27a5246883860ad4e6d7edae550548c504bce05a
imFaizanAhmed <imfaizanahmed6@gmail.com>
2024-02-06 00:54:06 +0500

    quard aka middleware added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { CreatorsModule } from 'src/modules';
+import { jwtConstants } from './constants';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [CreatorsModule],
+  imports: [CreatorsModule, JwtModule.register({
+    global: true,
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  }),],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreatorsService } from 'src/modules/creators/creators.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private creatorsService: CreatorsService) {}
+  constructor(
+    private creatorsService: CreatorsService,
+    private jwtService: JwtService,
+  ) {}
 
-    async signIn(email: string, pass: string): Promise<any> {
-        const user = await this.creatorsService.findOne(email);
-        if (user?.password !== pass) {
-          throw new UnauthorizedException();
-        }
-        const { password, ...result } = user;
-        // TODO: Generate a JWT and return it here
-        // instead of the user object
-        return result;
-      }
+  async signIn(email: string, pass: string): Promise<{ access_token: string }> {
+    const creator = await this.creatorsService.findOne(email);
+    if (creator?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = {
+      sub: creator.firstName + creator.lastName,
+      email: creator.email,
+    };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
